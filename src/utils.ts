85b2d13fd056a05d809027734f76908786676d93
Maher Abaza <maher.abaza@protonmail.com>
2023-09-30 17:22:31 +0300

    fix: format time

--- src/utils.ts
+++ src/utils.ts
@@ -18,7 +18,7 @@ export const formatTime = (seconds: number) => {
   if (isNaN(seconds)) {
     return `00:00`;
   }
-  const minutes = `${(seconds / 60).toFixed()}`;
-  const remainingSeconds = `${(seconds % 60).toFixed()}`;
+  const minutes = `${Math.floor(seconds / 60)}`;
+  const remainingSeconds = `${Math.floor(seconds % 60)}`;
   return `${minutes.padStart(2, "0")}:${remainingSeconds.padStart(2, "0")}`;
 };
